eea8e0b73d5ff54b7776cbfcca5b76de9dae122f
詹神 <2860021044@qq.com>
2026-05-12 14:55:51 +0800

    feat: new server

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,6 @@
-# --- 配置区 ---
-REMOTE_USER="root"
-REMOTE_HOST="1.95.137.119"
-REMOTE_PATH="/www/server/vflow-ai-frontend"
-SSH_KEY="/data/cxr25/zhx/id_ed25519"
-
-SSH_OPTS=""
-if [ -n "$SSH_KEY" ]; then
-    SSH_OPTS="-i $SSH_KEY"
-fi
-
-npm run build && \
-# 使用 GNU tar 或 BSD tar 清理生成
-COPYFILE_DISABLE=1 tar --format=ustar --exclude='.DS_Store' -zcvf dist.tar.gz ./dist && \
-scp $SSH_OPTS dist.tar.gz $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH && \
-ssh $SSH_OPTS $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_PATH && rm -rf ./dist && tar -xzf ./dist.tar.gz && echo '✅ 部署完成'"
-#rm dist.tar.gz
+sudo npm run build && \
+rm -f dist.tar.gz && \
+COPYFILE_DISABLE=1 tar --disable-copyfile --no-xattrs -czf dist.tar.gz --exclude='.DS_Store' ./dist && \
+scp dist.tar.gz root@180.76.239.114:/www/server/vflow-ai-frontend && \
+ssh root@180.76.239.114 'cd /www/server/vflow-ai-frontend && rm -rf ./dist && tar -xzf ./dist.tar.gz && echo "✅ 部署完成"'
+# rm -f dist.tar.gz
